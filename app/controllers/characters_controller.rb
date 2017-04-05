4925d8792168db2fc2598afbb312ed1ddc37f546
Project AD&D <user@email.com>
2017-04-05 15:10:38 +0000

    Created Character Controller -mm

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,15 +1,31 @@
 class CharactersController < ApplicationController
-  def index
-  end
-  
-  def new
-  end
-  
-  def create
-  end
-  
-  def edit
-  end 
-  
+    def new
+        @character = Character.new
+    end
+    
+    def index
+        @character = Character.all
+    end
+   
+    def show
+        @character = Character.find(params[:id])
+    end
+    
+    def create
+          @character = Character.new(character_params)
+        if @character.save
+            redirect_to @character
+        else
+            render 'new'
+        end
+    end
+    
+    private
+    def character_params
+        params.require(:character).permit(:name, :race_id, :job_id)
+    end
+    
 end
+  
+
 
