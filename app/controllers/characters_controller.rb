950c9add712b366329301d3fb741e24fa93b1ef9
Project AD&D <user@email.com>
2017-04-10 16:11:15 +0000

    Added CRUD methods for Characters controller and Character Show  -Daniel

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -20,6 +20,21 @@ class CharactersController < ApplicationController
         end
     end
     
+     def update
+        @character = Character.find(params[:id])
+        if @character.update(characters_params)
+            redirect_to @character
+        else
+            render 'edit'
+        end
+    end 
+    def destroy
+        @character = Character.find(params[:id])
+        @character.destroy
+        
+        redirect_to characters_path
+    end
+    
     private
     def character_params
         params.require(:character).permit(:name, :race_id, :job_id)
