939c897caefae6edcf3c5bb10c9cb701270f6649
Project AD&D <user@email.com>
2017-04-12 14:57:48 +0000

    Starting the job.index page -JR

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,20 @@
 class JobsController < ApplicationController
     def new
-end
+    end
+    def show
+        @job = Job.find(params[:id])
+    end
+    
+    def index
+        @jobs = Job.all
+    end
+    
+    private
+    def job_params
+         params.require(:job)
+         .permit(:name, :startingHP, :hitDice, :primaryAbility, :savingThrow1, 
+         :savingThrow2, :armorPref, :weaponPref, :profBonus, :skills, 
+         :startingEquip, :cantrip, :spellSlots, :spells)
+    end
+   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   
   resources :characters
+  resources :jobs
+  resources :race
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
