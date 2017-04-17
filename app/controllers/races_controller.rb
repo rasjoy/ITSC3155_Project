52d72588f14c112babe5a1fa09c78bfb33217ad9
Project AD&D <user@email.com>
2017-04-17 15:34:05 +0000

    Updated race controller -JR

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,2 +1,21 @@
 class RacesController < ApplicationController
+     def new
+    end
+    def show
+        @race = Race.find(params[:id])
+    end
+    
+    def index
+        @races = Race.all
+    end
+    
+    private
+    def job_params
+         params.require(:race)
+         .permit(:name, :abilityScore, :maxAge, :weight, :alignment, :size, 
+         :speed, :language1, :language2, :language3, :darkVision, :weaponPref, 
+         :armorPref, :extraAbility1, :extraAbility2, :extraAbility3, :extraAbility4, 
+         :extraAbility5)
+    end
+    
 end
