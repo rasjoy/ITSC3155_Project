12eade6c7c8482ab6be65cdf21a7d060bdec7cbb
Project AD&D <user@email.com>
2017-05-07 21:05:25 +0000

    increased fields on character view -ar

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -19,15 +19,53 @@ class CharactersController < ApplicationController
         end
         values.sort!
         @character = Character.new(character_params)
+        race = Race.find(@character.race_id)
+        job = Job.find(@character.job_id)
         @character.creator = current_user.name
         @character.user_id = current_user.id
-        @character.hp = 0
+        @character.hp = job.startingHP
         @character.constitution = values[0]
         @character.intelligence = values[1]
         @character.wisdom = values[2]
         @character.charisma = values[3]
         @character.strength = values[4]
         @character.dexterity = values[5]
+
+    
+    ## From Race db
+        @character.abilityScore = race.abilityScore
+        @character.maxAge = race.maxAge
+        @character.weight = race.weight
+        @character.alignment = race.alignment
+        @character.size = race.size
+        @character.speed = race.speed
+        @character.language1 = race.language1
+        @character.language2 = race.language2
+        @character.language3 = race.language3
+        @character.darkVision = race.darkVision
+        @character.weaponProf = race.weaponProf
+        @character.prof = race.prof
+        @character.extraAbility1 = race.extraAbility1
+        @character.extraAbility2 = race.extraAbility2
+        @character.extraAbility3 = race.extraAbility3
+        @character.extraAbility4 = race.extraAbility4
+        @character.extraAbility5 = race.extraAbility5
+    
+    ## From Job db
+        @character.startingHP = job.startingHP
+        @character.hitDice = job.hitDice
+        @character.primaryAbility = job.primaryAbility
+        @character.savingThrow1 = job.savingThrow1
+        @character.savingThrow2 = job.savingThrow2
+        @character.armorPref = job.armorPref
+        @character.weaponPref = job.weaponPref
+        @character.profBonus = job.profBonus
+        @character.skills = job.skills
+        @character.startingEquip = job.startingEquip
+        @character.spellSlots = job.spellSlots
+        @character.spells = job.spells
+        @character.cantrip = job.cantrip
+    
         if @character.save
             redirect_to @character
         else
